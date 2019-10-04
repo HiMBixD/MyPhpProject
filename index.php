560c4fa4c637e60795369e3777da31cb35ecf3c0
HiMBixD <tuananvip123@gmail.com>
2019-10-04 15:38:15 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,32 @@
 <h1>My first PHP page</h1>
 
 <?php
-echo "Hello World! lol hahahhahahaha";
-?>
+	echo "Show every from database";
+	$db = parse_url(getenv("DATABASE_URL"));
+	
+	$pdo = new PDO("pgsql:" . sprintf(
+	    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+	    $db["host"],
+	    $db["port"],
+	    $db["user"],
+	    $db["pass"],
+	    ltrim($db["path"], "/")
+									)
+				);
+	$sql= "SELECT studentname, course FROM registercourse";
 
+	$stmt= $pdo->prepare($sql);
+	$stmt->setFetchMode(PDO::FETCH_ASSOC);
+	$stmt->execute();
+	$resultSet= $stmt->fetchAll();
+?>
+<ul>
+	<?php 
+		foreach ($$resultSet as $row) {
+			echo "<li>".
+				$row["studentname"] . '--' . $row["course"] . "</li>";
+		}
+	 ?>
+</ul>
 </body>
 </html>
