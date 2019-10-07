c2e2b329e62905789c84e78f8b5c3f524cb6e35d
HiMBixD <tuananvip123@gmail.com>
2019-10-07 15:03:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 
 <h1>My first PHP page</h1>
 
+
 <?php
 	echo "Show every from database";
 	$db = parse_url(getenv("DATABASE_URL"));
@@ -17,10 +18,28 @@
 	    ltrim($db["path"], "/")
 									)
 				);
-	$sql1= "INSERT INTO RegisterCourse values (3,'name','namecourse','6/10/1997','gender','fav')";
-	$stmt1= $pdo->prepare($sql1);
-	$stmt1->setFetchMode(PDO::FETCH_ASSOC);
-	$stmt1->execute();
+?>
+<form action="/action_page.php">
+ID: <input type="text" name="ID" value=""><br>
+Name: <input type="text" name="Name" value=""><br>
+Name Course: <input type="text" name="Course" value=""><br>
+Dob: <input type="text" name="Dob" value=""><br>
+Gender: <input type="text" name="Gender" value=""><br>
+Fav: <input type="text" name="Fav" value=""><br>
+<input type="submit" value="Submit">
+</form>
+<?php
+`(int)$num`
+	if(isset($_POST['ID']))
+	{
+		$ID=(int)$_POST['ID'];
+		$add=[$ID,$_POST['Name'],$_POST['Course'],$_POST['Dob'],$_POST['Gender'],$_POST['Fav']];
+		$sql1= "INSERT INTO RegisterCourse values ($add[0],'$add[1]','$add[2]','$add[3]','$add[4]','$add[5]')";
+		$stmt1= $pdo->prepare($sql1);
+		$stmt1->setFetchMode(PDO::FETCH_ASSOC);
+		$stmt1->execute();
+	}
+	
 	$sql= "SELECT * FROM RegisterCourse";
 
 	$stmt= $pdo->prepare($sql);
