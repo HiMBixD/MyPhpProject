68d726b5f7837916aaba7e66d2a83ceeebc0a666
HiMBixD <tuananvip123@gmail.com>
2019-10-07 15:05:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 									)
 				);
 ?>
-<form action="/action_page.php">
+<form action="/index.php">
 ID: <input type="text" name="ID" value=""><br>
 Name: <input type="text" name="Name" value=""><br>
 Name Course: <input type="text" name="Course" value=""><br>
